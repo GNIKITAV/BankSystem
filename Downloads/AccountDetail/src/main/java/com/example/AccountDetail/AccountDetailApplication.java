b8105285c7b522dcfb14f62a2c50b401e267d23e
GNIKITAV <gaikwadnikita230@gmail.com>
2022-02-01 11:16:28 +0530

    first commit

---
+++ Downloads/AccountDetail/src/main/java/com/example/AccountDetail/AccountDetailApplication.java
@@ -0,0 +1,14 @@
+package com.example.AccountDetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountDetailApplication.class, args);
+		System.out.println("this is acc");
+	}
+
+}
